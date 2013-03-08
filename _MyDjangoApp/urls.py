797090dc05c5280e924c48a71d22855fb6baff14
Rafael Neves <rafaeljneves@gmail.com>
2013-03-08 09:37:45 -0300

    padronizando caminho urls

--- _MyDjangoApp/urls.py
+++ _MyDjangoApp/urls.py
@@ -1,13 +1,23 @@
 from django.conf.urls import patterns, include, url
 
+from polls import views
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', '_MyDjangoApp.views.home', name='home'),
-    # url(r'^_MyDjangoApp/', include('_MyDjangoApp.foo.urls')),
+    # ex: /polls/
+    url(r'^polls/$', views.index , name='index'),
+    # ex.: /polls/5
+    url(r'^polls/(?P<poll_id>\d+)/$', views.details, name='details'),
+    # ex.: /polls/5/results
+    url(r'^polls/(?P<poll_id>\d+)/results/$', views.results, name='results'),
+    # ex.: /polls/5/vote
+    url(r'^polls/(?P<poll_id>\d+)/vote/$', views.vote,  name='vote'),
+    # ex.: /time
+    url(r'^time/$', views.hora_certa, name='hora_certa'),
+
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
