66d1a369e4357bead2bab5b44a822ae6c62584a3
rafaeljneves <rafaeljneves@gmail.com>
2013-10-01 16:51:04 -0300

    using  RequestContext

--- polls/views.py
+++ polls/views.py
@@ -13,7 +13,8 @@ from polls.models import Poll, Choice
 
 def index(request):
     poll_list = Poll.objects.order_by('data_publicacao')
-    return render_to_response('index.html', {'poll_list': poll_list})
+    return render_to_response('index.html', RequestContext(request, {
+                                                'poll_list': poll_list}))
 
 
 @ensure_csrf_cookie
