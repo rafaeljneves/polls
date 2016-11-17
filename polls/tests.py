9a171ac0ae1f671fc47ed3bee11e4821bd573c5a
Rafael Neves <rafaeljneves@gmail.com>
2016-11-17 02:08:20 +0000

    commit usando eclipse che!

--- polls/tests.py
+++ polls/tests.py
@@ -2,7 +2,8 @@
 This file demonstrates writing tests using the unittest module. These will pass
 when you run "manage.py test".
 
-Replace this with more appropriate tests for your application.
+Replace this wi
+th more appropriate tests for your application.
 """
 
 from django.test import TestCase
