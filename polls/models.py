0e281d675e7144683e429b0543ada891f655b3b8
rafaeljneves <rafaeljneves@gmail.com>
2017-05-02 16:13:52 -0300

    configurando django tests

--- polls/models.py
+++ polls/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
+
 class Poll(models.Model):
+    """
+    Poll eh um objeto de votacao
+    """
+
     questao = models.CharField(max_length=200)
-    data_publicacao = models.DateTimeField('Data Publicacao')
+    data_publicacao = models.DateTimeField(auto_now_add=True, blank=True) #'Data Publicacao'
+
+    def votar(self):
+        return 'O que voce achou do "%s"?' % self.questao
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
--- polls/tests.py
+++ polls/tests.py
@@ -5,27 +5,36 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.http import HttpResponseRedirect, HttpResponse
-#from django.test import TestCase
-#from polls.views import vote
-import unittest
+from django.test import TestCase
+from django.test.client import Client
+from polls.models import Poll
 
 
-class SimpleTest(unittest.TestCase):
+class SimpleTest(TestCase):
     def setUp(self):
-        self.test = 1
-        pass
+        self.questao_teste =  Poll.objects.create(questao="teste")
+        self.data_pub_teste = Poll.objects.create(data_publicacao="02-05-2017")
+        self.client = Client()
 
-    def test_voto_nao_deve_ser_nulo(self):
-        """
-        dado um voto nulo deve falhar
-        """
-        self.test = 1#vote(request=0)
-        self.assertFalse(self.test, "test_voto_nao_deve_ser_nulo" )
 
-    def tearDown(self):
-        self.test = 0
-        pass
+    def test_deve_criar_questao_para_votar(self):
+        """
+        deve formular questao para votar
+        """
+        self.assertEqual(self.questao_teste.votar(), 'O que voce achou do "teste"?')
+
+    def test_deve_acessar_questao(self):
+        """
+        Faz uma requisicao GET e
+        verifica se a resposta foi 200 OK.
+        """
+        response = self.client.get('/polls/1/')
+        self.failUnlessEqual(response.status_code, 200)
+
+
+    #def tearDown(self):
+    #    self.test = 0
+    #    pass
 
 
 
