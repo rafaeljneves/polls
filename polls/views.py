037d3983e3b5df2622be3b1191f65872f64bb275
rafaeljneves <rafaeljneves@gmail.com>
2017-05-04 16:43:22 -0300

    PEP8

--- polls/views.py
+++ polls/views.py
@@ -5,51 +5,50 @@ from django.core.urlresolvers import reverse
 from django.template.context_processors import csrf
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.template.response import TemplateResponse
-
 from polls.models import Poll, Choice
 from time import gmtime, strftime
 
 
-
-def index(request):
+def index():
     poll_list = Poll.objects.order_by('data_publicacao')
-
     return render_to_response('index.html', {'poll_list': poll_list})
+
 
 @ensure_csrf_cookie
 def details(request, poll_id):
     poll = get_object_or_404(Poll, pk=poll_id)
-    c={}
+    c = {}
     c.update(csrf(request))
-
     return TemplateResponse(request, 'details.html', {'poll_id': poll,
-                                                   'c':c})
+                                                      'c': c})
+
 
 def results(request, poll_id):
     poll = get_object_or_404(Poll, pk=poll_id)
-    # return render_to_response('results.html', {'poll_id':poll})
-    return render(request, 'results.html', {'poll_id':poll})
+    return render(request, 'results.html', {'poll_id': poll})
 
 
 def vote(request, poll_id):
-    p =get_object_or_404(Poll, pk=poll_id)
-    c={}
+    p = get_object_or_404(Poll, pk=poll_id)
+    c = {}
     c.update(csrf(request))
     try:
         selected_choice = p.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        #exibe novamente o formulario  details
-        return render(request, 'details', {'poll_id':p,
-                                                'error_message': "favor selecione uma opcao.",
-                                               })
+        # exibe novamente o formulario  details
+        return render(request, 'details', {
+                                           'poll_id': p,
+                                           'error_message': "favor selecione uma opcao.",
+                                          }
+                      )
     else:
-        selected_choice.votos +=1
+        selected_choice.votos += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('results', args=(p.id,)))
         # return render(request, 'results.html', {'c':c})
 
 
-def hora_certa(request):
+def hora_certa():
     data_hora = strftime("%Y-%m-%d %H:%M:%S", gmtime())
     html = "<html><body>Hora Certa: %s</body></html>" % data_hora
     return HttpResponse(html)
